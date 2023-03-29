c000686efa3cb25c560fb3598410097ff0a2b82d
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-03-29 14:11:05 -0300

    feat: add default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
+
 function App() {
 
   return (
-    <div className="App">
-      <h1>HELLO</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Hello World</h1>
+
+    </ThemeProvider>
   )
 }
 

17637bc755cca784b6211645e5ad1e5c0423e451
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-03-30 09:23:07 -0300

    feat: create styled table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,31 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsTable, TransanctionsContainer } from "./style";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
-    </div>)
+
+      <TransanctionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento</td>
+              <td><PriceHighLight variant="income">R$ 12.000,00</PriceHighLight></td>
+              <td>Venda</td>
+              <td>13/04/2013</td>
+            </tr>
+            <tr>
+              <td width="50%">X-Burger</td>
+              <td><PriceHighLight variant="outcome">- R$ 12.000,00</PriceHighLight></td>
+              <td>Alimentação</td>
+              <td>13/04/2013</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransanctionsContainer>
+    </div>
+  )
 }

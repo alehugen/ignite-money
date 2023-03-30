1a1c02330d0297bb70f21c4e0fac94de1831b55d
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-03-30 12:21:37 -0300

    feat: create and style modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
+import * as Dialog from '@radix-ui/react-dialog'
 import logoIMG from '../../assets/LOA_LOGO.png'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoIMG} alt="logotipo" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )

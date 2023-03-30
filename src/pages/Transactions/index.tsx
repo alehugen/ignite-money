4663f5965071ea671a6456966f11e9d0b32dccf1
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-03-30 14:27:37 -0300

    feat: create formatter functions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsTable, TransanctionsContainer } from "./style";
 import { TransactionsContext } from "../../contexts/TrasactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext);
@@ -21,9 +22,12 @@ export function Transactions() {
                 return (
                   <tr key={transaction.id}>
                     <td width="50%">{transaction.description}</td>
-                    <td><PriceHighLight variant={transaction.type}>{transaction.price}</PriceHighLight></td>
+                    <td><PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight></td>
                     <td>{transaction.category}</td>
-                    <td>{transaction.createdAt}</td>
+                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                   </tr>
                 )
               })

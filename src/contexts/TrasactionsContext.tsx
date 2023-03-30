dd752d81f57682e870b1df732b2678c87c412a2d
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-03-30 15:52:22 -0300

    feat: add filter for transactions

--- src/contexts/TrasactionsContext.tsx
+++ src/contexts/TrasactionsContext.tsx
@@ -11,6 +11,7 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TransactionContextProps {
@@ -22,19 +23,24 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 export function TransactionProvider({ children }: TransactionContextProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function loadTransacions() {
-    const response = await fetch('http://localhost:3333/transactions')
+  async function fetchTransactions(query?: string) {
+    const url = new URL('http://localhost:3333/transactions');
+    if (query) {
+      url.searchParams.append('q', query)
+    }
+
+    const response = await fetch(url)
     const data = await response.json()
 
     setTransactions(data);
   }
 
   useEffect(() => {
-    loadTransacions();
+    fetchTransactions();
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   )
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./style";
 import { useForm } from "react-hook-form";
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TrasactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -11,12 +13,17 @@ const searchFormSchema = z.object({
 type SearchFromInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFromInputs>({
+  const { fetchTransactions } = useContext(TransactionsContext);
+
+  const { register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<SearchFromInputs>({
     resolver: zodResolver(searchFormSchema),
   });
 
-  function handleSearchTransactions(data: SearchFromInputs) {
-    console.log(data)
+  async function handleSearchTransactions(data: SearchFromInputs) {
+    await fetchTransactions(data.query)
   }
 
   return (
